77c4cb2db9721dd907da016727ba64174d7d0d96
James Doyle <james2doyle@gmail.com>
2019-03-16 17:10:22 -0700

    update deps and simplify everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
@@ -18,11 +17,6 @@ module.exports = {
   ],
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true, // set to true if you want JS source maps
-      }),
       new OptimizeCSSAssetsPlugin({}),
     ],
   },
@@ -37,15 +31,18 @@ module.exports = {
       {
         test: /\.(scss|css)$/,
         use: [
-          MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: MiniCssExtractPlugin.loader,
             options: {
               minimize: {
                 safe: true,
               },
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../',
             },
           },
+          'css-loader',
           {
             loader: 'sass-loader',
             options: {},
